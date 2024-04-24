f6938e9a51a763f2c60a1af1e5524a936cb43d02
Felix Ackon <ackon3000@yahoo.com>
2024-04-24 16:42:37 -0400

    changed the gen cohort function to make it work

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import argparse
 
-from shapley.shapley import asset_alloc
+from shapley.shapley import gen_cohorts, shap_alloc
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
@@ -9,6 +9,7 @@ if __name__=="__main__":
     parser.add_argument("--output_file", type=str, help="Path to the desired output file.")
     parser.add_argument("--alpha", type=float, help="1-quantile.", default=0.05)
     parser.add_argument("--num_scen", type=int, help="Number of scenarios.", default=500)
+    parser.add_argument("--num_cohorts", type=int, help="Number of cohorts.", default=3)
     args = parser.parse_args()
 
     #Static variables
@@ -26,5 +27,8 @@ if __name__=="__main__":
 
     output_file = args.output_file
 
-    result = asset_alloc(num_scen, alpha, folder_path, area_fname)
+    num_cohorts = args.num_cohorts
+
+    scen_df = gen_cohorts(num_scen, num_cohorts, folder_path, area_fname)
+    result = shap_alloc(num_scen, alpha, num_cohorts, scen_df)
     result.to_csv(output_file)
